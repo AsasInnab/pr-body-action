38677869282fa036ba3177db202553d22fed9742
Tymoteusz Stępień <57900160+403-html@users.noreply.github.com>
2023-03-29 18:00:36 +0200

    feat: add option for custom pr, and remove

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ try {
   const { context } = github;
   const githubToken = core.getInput("GITHUB_TOKEN");
   const body = core.getInput("body");
+  const providedPrNumber = core.getInput("pr_number");
 
   if (context.payload.pull_request == null) {
     core.setFailed("No pull request found");
-    return;
   }
   if (!githubToken) {
     core.setFailed("GITHUB_TOKEN input is required");
-    return;
   }
   if (!body) {
     core.setFailed("body input is required");
-    return;
   }
 
-  const { number: prNumber } = context.payload.pull_request;
+  const { number: currentPrNumber } = context.payload.pull_request;
+  const prNumber = providedPrNumber || currentPrNumber;
+
   const octokit = new github.GitHub(githubToken);
   octokit.pulls.update({
     ...context.repo,
     pull_number: prNumber,
-    body
+    body,
   });
 } catch (error) {
   core.setFailed(error.message);
